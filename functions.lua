558b55dec4f7948d8d311663047409ae2b607a77
andawi <andanator@gmail.com>
2014-12-31 17:40:24 +0100

    initial upload

---
+++ functions.lua
@@ -0,0 +1,13 @@
+--------------------------------------------------------
+-- Blood Legion Raidcooldowns - Functions --
+--------------------------------------------------------
+if not BLCD then return end
+local BLCD = BLCD
+local CB = LibStub("LibCandyBar-3.0")
+local Elv = IsAddOnLoaded("ElvUI")
+local ACD = LibStub("AceConfigDialog-3.0") -- Also for options panel
+
+if(Elv) then
+	E, L, V, P, G =  unpack(ElvUI);
+end
+
